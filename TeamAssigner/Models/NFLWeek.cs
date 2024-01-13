8f2879defd251840e673ba24c1150b84e079bf96
Andy Salmela <asalmela@northmarq.com>
2024-01-12 20:52:57 -0600

    fix week checker

---
+++ TeamAssigner/Models/NFLWeek.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamAssigner.Models
+{
+    public sealed class NFLWeek
+    {
+        public string text { get; set; }
+        public int number { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+    }
+}
